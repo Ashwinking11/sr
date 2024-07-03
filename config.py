8354615752fa68815ff6f5881ca4e7a9ee487caa
ashwinrin77 <161035206+ashwinrin77@users.noreply.github.com>
2024-07-03 06:28:17 -1200

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,5 @@
 BOT_TOKEN = "7245789879:AAEgKB0FOWQ5g1laVULc-vfniUrrBWxQHTA"
 API_ID = 28015531
 API_HASH = "2ab4ba37fd5d9ebf1353328fc915ad28"
+# MongoDB connection URI
+MONGODB_URI = 'mongodb://localhost:27017'
