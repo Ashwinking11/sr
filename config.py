18b3bf57ad636dddd5fbd1cdc291118023275650
ashwinrin77 <161035206+ashwinrin77@users.noreply.github.com>
2024-07-03 07:04:50 -1200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-# config.py
+# Telegram Bot API credentials
 BOT_TOKEN = "7245789879:AAEgKB0FOWQ5g1laVULc-vfniUrrBWxQHTA"
 API_ID = 28015531
 API_HASH = "2ab4ba37fd5d9ebf1353328fc915ad28"
+
 # MongoDB connection URI
 MONGODB_URI = 'mongodb+srv://haribotx:haribotx@cluster0.i3skil4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
