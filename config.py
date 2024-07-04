e61eecacaac0101d7affacdecaa3728a9a4622d3
ashwinrin77 <161035206+ashwinrin77@users.noreply.github.com>
2024-07-04 09:01:38 -1200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "7291721040:AAEEfVKQ2thZJkR0jjn1DTeGbt3Cp0HX-u4"
+BOT_TOKEN = "7045821543:AAFBxSwnRWzoPv86ANtgjr4kxt0blDNrsRU"
 API_ID = "28015531"
 API_HASH = "2ab4ba37fd5d9ebf1353328fc915ad28"
 DATABASE_URL = "mongodb+srv://haribotx:haribotx@cluster0.i3skil4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
